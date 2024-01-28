519c9465bcb1416e425608b5cb8608f06f86d4c3
ricardojonathanromero <ricardo.jonathan.romero@gmail.com>
2024-01-27 23:49:43 -0600

    feat: update logger formatter

--- backend/utilities/middlewares/logger.go
+++ backend/utilities/middlewares/logger.go
@@ -1,30 +1,17 @@
 package middlewares
 
 import (
-	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/ricardojonathanromero/order-products-vue-go/backend/utilities/logger"
+	"time"
 )
 
-func NewCustomLogger(log logger.Logger) middleware.RequestLoggerConfig {
-	return middleware.RequestLoggerConfig{
-		LogURI:    true,
-		LogStatus: true,
-		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
-			log.AddFields(map[string]any{
-				"start_time":     v.StartTime,
-				"request_id":     c.Request().Header.Get(echo.HeaderXRequestID),
-				"host":           c.Request().Host,
-				"content_length": v.ContentLength,
-				"protocol":       c.Request().Proto,
-				"real_ip":        c.RealIP(),
-				"method":         c.Request().Method,
-				"uri":            c.Request().RequestURI,
-				"status":         v.Status,
-			})
-			log.Info("request")
-
-			return nil
-		},
+func NewCustomLogger() middleware.LoggerConfig {
+	return middleware.LoggerConfig{
+		Skipper: middleware.DefaultSkipper,
+		Format: `{"time":"${time_rfc3339_nano}","id":"${id}","remote_ip":"${remote_ip}",` +
+			`"host":"${host}","method":"${method}","uri":"${uri}","user_agent":"${user_agent}",` +
+			`"status":${status},"error":"${error}","latency":${latency},"latency_human":"${latency_human}"` +
+			`,"bytes_in":${bytes_in},"bytes_out":${bytes_out}}` + "\n",
+		CustomTimeFormat: time.RFC3339,
 	}
 }
